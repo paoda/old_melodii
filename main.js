ca6613344ffdae7f71c509e5367374a46c6fbb46
Rekai Musuka <Rekai Musuka>
2016-12-30 03:22:31 -0600

    wrote basic main.js worked on bare-bones index.html created css and js for index.html and updated package.json once again

---
+++ main.js
@@ -0,0 +1,34 @@
+const electron  = require('electron');
+const path = require('path');
+const url = require('url');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 640, height: 360})
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  })
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+  app.quit();
+})
+
+//For OSX
+app.on('activate', function() {
+  if (mainWindow == null) {
+    creteWindow();
+  }
+})
