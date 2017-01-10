65fcc4728059f4f9dfaf40ab3c328631f1a2366d
Rekai Musuka <Rekai Musuka>
2017-01-09 23:50:58 -0600

    began making <a> tag more button like.
    outputting directory variable input to html now for diagnostic sakes
    
    am going to bed now exited for what the future has in store.

--- dependencies/getDir.js
+++ dependencies/getDir.js
@@ -2,4 +2,9 @@ const {dialog} = require('electron').remote;
 var directory;
 function dirBtn() {
      directory = dialog.showOpenDialog({properties: ['openFile', 'openDirectory']});
+    
+     directory = directory.toString();
+
+     document.getElementById('text').innerHTML = directory;
 }
+
