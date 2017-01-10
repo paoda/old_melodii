24713e7b31bd38a4358eeac6e21c5b6b93ef343c
Rekai Musuka <Rekai Musuka>
2017-01-10 09:03:24 -0600

    installed web-fs

--- dependencies/getDir.js
+++ dependencies/getDir.js
@@ -1,10 +1,7 @@
 const {dialog} = require('electron').remote;
 var directory;
+
 function dirBtn() {
      directory = dialog.showOpenDialog({properties: ['openFile', 'openDirectory']});
-    
-     directory = directory.toString();
-
-     document.getElementById('text').innerHTML = directory;
+     directory = directory.toString();  
 }
-
