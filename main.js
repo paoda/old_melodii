b9073496784d6df52d9e8776002a555225fdcabe
Paoda <musukarekai@gmail.com>
2017-01-09 21:38:36 -0600

    Majority of things are broken whoops.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const electron  = require('electron');
+const electron = require('electron');
+const ipc = require('electron').ipcMain
+const dialog = require('electron').dialog
 const path = require('path');
 const url = require('url');
 
@@ -8,30 +10,30 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 640,
-    height: 360
-  })
+    mainWindow = new BrowserWindow({
+        //frame: false,
+        width: 640,
+        height: 360
+    })
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file',
-    slashes: true
-  }))
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  })
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    })
 }
 
 app.on('ready', createWindow);
-app.on('window-all-closed', function () {
-  app.quit();
+app.on('window-all-closed', function() {
+    app.quit();
 })
-
 //For OSX
 app.on('activate', function() {
-  if (mainWindow == null) {
-    createWindow();
-  }
+    if (mainWindow == null) {
+        createWindow();
+    }
 })
