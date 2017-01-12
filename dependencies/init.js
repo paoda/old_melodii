62e4d41e0debc208f35974a20fd639678dcb9b59
Paoda <musukarekai@gmail.com>
2017-01-11 21:16:04 -0600

    Connect aurora.js files to init and index files
    got rid of prism.js that snuck into the aurora.js folder
    
    created musicPlayer.js for when it's needed.

--- dependencies/init.js
+++ dependencies/init.js
@@ -5,6 +5,13 @@ const browserWindow = remote.BrowserWindow;
 const fs = require('fs');
 const process = require('os');
 
+//All Aurora.js libraries
+const av = require('aurora.js/aurora.js');
+    require('aurora.js/aac.js');
+    require('aurora.js/alac.js');
+    require('aurora.js/flac.js');
+    require('aurora.js/mp3.js');
+    
 var userOS = process.platform();
 console.log('Client OS = ' + userOS);
 
