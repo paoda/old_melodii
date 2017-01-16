808103c6cb86f0bbc95e9d2666acee5bbc79ceef
Paoda <musukarekai@gmail.com>
2017-01-15 21:27:01 -0600

    cleared musicPlayer.js

--- dependencies/musicPlayer.js
+++
@@ -1,6 +0,0 @@
-var player;
-
-function music(){
-    player = AV.Player.fromFile('C:\\Users\\Rekai\\Google Drive\\Music\\supercell\\[ASL] supercell - Today Is A Beautiful Day [FLAC] [w Scans]\\09 Hoshi ga Matataku Konna Yoru ni.flac');
-    player.play();
-}
