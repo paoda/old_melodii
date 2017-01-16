1ba6ecd538ab6f4a5c96de2d050cb57154f3f1c1
Rekai Musuka <musukarekai@gmail.com>
2017-01-15 22:08:42 -0600

    removed window close and minimize functions with event listners.

--- dependencies/windowControl.js
+++ dependencies/windowControl.js
@@ -1,7 +1,18 @@
-function quit(){
+/*function quit(){
     app.quit();
 }
 function minimize() {
     browserWindow.getFocusedWindow().minimize();
 }
+*/
 // Don't comment on this. Just let it be please.
+
+var quit = document.getElementById('close');
+var minimize = document.getElementById('minimize');
+
+quit.addEventListener('click', () => {
+	app.quit();
+})
+minimize.addEventListner('click', () => {
+	browserWindow.getFocusedWindow().minimize();
+})
