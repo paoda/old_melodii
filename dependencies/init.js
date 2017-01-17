535609687b2c6e3c0ecf5408b774c6865b6e8e42
Paoda <musukarekai@gmail.com>
2017-01-16 19:44:06 -0600

    added all necessary aurora.js files via npm.
    They still don't work though.
    
    need to figure how to play local files in node.js

--- dependencies/init.js
+++ dependencies/init.js
@@ -6,7 +6,12 @@ const fs = require('fs');
 const os = require('os');
 
 //All Aurora.js libraries
-
+const AV = require('av');
+    require('mp3');
+    require('flac.js');
+    require('alac');
+    require('aac');
+    require('ogg.js');
 var userOS = os.platform();
 console.log('Client OS = ' + userOS);
 
