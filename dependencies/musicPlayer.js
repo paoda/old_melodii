a25fd6fd9138544005083d1981707401c2c42647
Paoda <musukarekai@gmail.com>
2017-01-16 21:49:11 -0600

    made example of current issues
    musicPlayer.js shows two similar functions that sould in 'theory' play a file. but the aurora.js one doesn't because????

---
+++ dependencies/musicPlayer.js
@@ -0,0 +1,9 @@
+function works() {
+    var play = new Audio('C:\\Users\\Rekai\\Google Drive\\Music\\Aimer\\Ninelie\\01 ninelie.mp3');
+    play.play();
+}
+
+function broken() {
+    var playdough = AV.Player.fromURL('C:\\Users\\Rekai\\Google Drive\\Music\\Aimer\\Ninelie\\01 ninelie.mp3');
+    playdough.play();
+}
