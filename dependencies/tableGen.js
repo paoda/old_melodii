fdc868f4eadb8d3f98f83009887ce4bfdee3fac4
Rekai Musuka <musukarekai@gmail.com>
2017-01-26 08:52:48 -0600

    plans to use javascript to create and add to table removed template

--- dependencies/tableGen.js
+++ dependencies/tableGen.js
@@ -1,14 +1,5 @@
-//http://stackoverflow.com/questions/15164655/generate-html-table-from-2d-javascript-array
-var table;
-function getTable(directory) {
-    let result = ["<table border=1'>"];
-    for(let row of directory) {
-        result.push("<tr>");
-        for(let cell of row){
-            result.push(`<td>${cell}</td>`);
-        }
-        result.push("</tr>");
-    }
-    result.push("</table>");
-    return result.join('\n');
-  }
+var table = document.getElementById('songTable');
+
+function makeTable() {
+
+}
