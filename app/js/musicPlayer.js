3a0ab9759cf0e1d5cf952f6953785cf380396219
Paoda <musukarekai@gmail.com>
2017-04-13 19:48:31 -0500

    began work on melodii class

--- app/js/musicPlayer.js
+++ app/js/musicPlayer.js
@@ -1,16 +1,52 @@
-var musicPlayer = new Audio(); //Audio Player
+var musicPlayer = new Audio(); //Audio musicPlayer
 
-function currentSong(location, artist, album, albumArt, year, genre, time) {
-    //properties
-    this.location = location;
-    this.artist = artist;
-    this.alum = album;
-    this.year = year;
-    this.genre = genre;
-    this.time = time;
+'use strict';
 
-    //methods
-    this.loadSong() {
-        musicPlayer.load(location);
+class melodii {
+    constructor(location) {
+        this.location = location;
+    }
+
+    metadata() {
+        let fileStream = fs.createReadStream(this.location);
+        let metadataParser = mm(fileStream, (err, metadata) => {
+            if (err) throw err;
+            console.log(metadata);
+            this.metadata = metadata;
+        })
+        filestream.close();
+    }
+
+    createAlbumArt() {
+        if (this.metadata.picture.length > 0) {
+            let picture = metadata.picture[0];
+            let url = URL.createObjectURL(new Blob([picture.data], {
+                'type': 'image/' + picture.format
+            }));
+            this.albumArt = url;
+        } else {
+            throw "Metadata has Album Art";
+        }
+    }
+
+    load() {
+        musicPlayer.load(this.location);
+    }
+    play() {
+        musicPlayer.play();
+    }
+    volUp() {
+        musicPlayer.volume -= 0.02;
+        //volRange.value = musicPlayer.volume;
+        console.log('New Volume:' + musicPlayer.volume);
+    }
+    volDown() {
+        musicPlayer.volume += 0.02;
+        //volRange.value = musicPlayer.volume;
+        console.log('New Volume:' + musicPlayer.volume);
+    }
+    updateVolume() { //used for slider control
+       // musicPlayer.volume = volRange.value;
+        console.log('new Volume:' + musicPlayer.volume);
     }
 }
