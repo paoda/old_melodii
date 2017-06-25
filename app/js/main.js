83646897fb1b869b4a3451eb534551285240e3a9
Paoda <musukarekai@gmail.com>
2017-06-24 20:49:01 -0500

    Made Progress towards fixing .footer CSS Issues
    Reorganized melodii.css

--- app/js/main.js
+++ app/js/main.js
@@ -10,10 +10,12 @@ if (fs.existsSync('./app/user/songs.mld')) { //Does songs.mld Exist?
         songs = data.toString('utf8'); //readFile will read .mld as a UInt8Array
         songs = songs.split(',\n'); //Each file name is separated by a , and then a newline
 
+        //Loads Entire songs.mld file, maybe not the most efficient, especially since I check only 1 file before discarding the entire thing.
+
         if (fs.existsSync(songs[Math.round(Math.random() * songs.length + 1)])) {
-            console.log('songs.mld passed Rudimentary Test. Files Loaded');
+            console.log('songs.mld passed rudimentary Test. Files Loaded');
         } else {
-            console.log('songs.mld failed rudimentary test. Reselect Default Directory');
+            window.alert('songs.mld failed rudimentary test. Reselect Default Directory');
             songs = null;
         }
     })
