27233449bd5bdd7fd13df55b634a2d979006ea9a
Paoda <musukarekai@gmail.com>
2017-06-27 18:27:01 -0500

    basic last.fm support
    need to save token so repeated use is possible + fix lastfm.getToken() so that it will only close the window once it's for sure gotten the token

---
+++ app/js/lastfm.js
@@ -0,0 +1,70 @@
+class LastFM {
+    constructor() {
+        this.api_key = 'API_KEY_HERE';
+        this.secret = 'SECRET_KEY_HERE';
+
+        const lastFmAPI = require('lastfmapi');
+         this.lfm = new lastFmAPI({
+            'api_key': this.api_key,
+            'secret': this.secret
+        })
+    }
+    getToken() {
+        let BrowserWindow = remote.BrowserWindow;
+
+        let win = new BrowserWindow({
+            width: 800,
+            height: 600
+        });
+
+        win.webContents.on('did-finish-load', () => {
+            win.show();
+            win.focus();
+        })
+        win.loadURL(`http://www.last.fm/api/auth/?api_key=${this.api_key}`);
+
+        win.webContents.on('will-navigate', (e, url) => {
+            let match = url.match(/token=(.*)/g);
+            this.token = match[0].substring(6, 38);
+            win.close();
+
+            let token = ;
+            settings.saveSettings(token);
+            this.authenticate();
+        })
+    }
+    authenticate() {
+        this.lfm.authenticate(this.token, (err, session) => {
+            if (err) throw err;
+            console.log(session);
+        })
+
+         musicPlayer.onended = () => this.scrobble(); //Once Authenicated Scobbling can be enabled.
+         musicPlayer.onloadedmetadata = () => {
+             seekRange.max = (melodii.metadata.format.duration == null) ? ~~musicPlayer.duration : ~~melodii.metadata.format.duration;
+             this.nowPlaying(); //Once Authenticated, nowPlaying can be enabled. //TODO Make autonomous
+         }
+    }
+    scrobble() {
+        this.lfm.track.scrobble({
+            'artist': melodii.metadata.common.artist,
+            'track': melodii.metadata.common.title,
+            'timestamp': ~~((new Date()).getTime() / 1000) - ((melodii.metadata.format.duration == null) ? ~~musicPlayer.duration : ~~melodii.metadata.format.duration)
+        }, (err, scrobble) => {
+            if (err) console.error(err);
+            console.log("Scrobbling Successful");
+        })
+    }
+    nowPlaying() {
+        this.lfm.track.updateNowPlaying({
+                'artist': melodii.metadata.common.artist,
+                'track': melodii.metadata.common.title,
+                'album': melodii.metadata.common.album,
+            }, (err, nowPlaying) => {
+            if (err) console.error(err);
+            console.log("Now Playing Successful");
+        })
+    }
+}
+
+var lastfm = new LastFM();
