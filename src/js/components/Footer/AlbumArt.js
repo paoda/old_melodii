2668d1646b1e349342c36495ae0e1fe276d6bb97
Paoda <musukarekai@gmail.com>
2017-08-17 23:20:37 -0500

    no album art image + Vertical Centering of Header Txt

--- src/js/components/Footer/AlbumArt.js
+++ src/js/components/Footer/AlbumArt.js
@@ -1,11 +1,16 @@
 'use strict';
 import React from 'react';
+import noalbumart from '../../../img/noalbumart.png';
 
 export default class AlbumArt extends React.Component {
+    constructor() {
+        super();
+        this.state = {albumart: false, albumArtLocation: noalbumart};
+    }
     render() {
         return (
             <div id='albumContainer'>
-                <img id='albumImg'></img>
+                <img src={this.state.albumArtLocation} id='albumImg'></img>
             </div>
         )
     }
