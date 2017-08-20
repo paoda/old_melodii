1ff629eaaf71ecff20d3869525655e3aa5ee164d
Paoda <musukarekai@gmail.com>
2017-08-20 00:33:33 -0500

    Main Window is hidden until webpage has loaded entirely

--- dist/main.js
+++ dist/main.js
@@ -9,20 +9,19 @@ const url = require('url');
 let mainWindow; //needs global reference
 
 function createWindow() {
-    mainWindow = new BrowserWindow({ width: 800, height: 600, frame: false});
-
+    mainWindow = new BrowserWindow({ width: 800, height: 600, frame: false, show: false});
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'app/index.html'),
         protocol: 'file',
         slashes: true
     }));
-
-    //mainWIndow.webContents.openDevTools();
-
+    mainWindow.webContents.on('did-finish-load', () => mainWindow.show());
+    //mainWindow.webContents.openDevTools();
     mainWindow.on('closed', () => mainWindow = null);
 }
 
 app.on('ready', createWindow);
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
